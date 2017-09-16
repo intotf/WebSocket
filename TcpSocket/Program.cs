abd626b79f5aab74ec4b50872ee6e8b9a9ae1de6
intotf <intotf@163.com>
2017-09-16 23:09:49 +0800

    WebSocket

---
+++ TcpSocket/Program.cs
@@ -0,0 +1,19 @@
+﻿using NetworkSocket.Fast;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpSocket
+{
+    class Program
+    {
+        static Main(string[] args)
+        {
+            FastTcpClient client = new FastTcpClient();
+            client.Connect("10.0.3.88", 1850);
+
+        }
+    }
+}

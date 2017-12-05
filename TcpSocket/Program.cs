1f1c6632560f41fac77ca57deef7c6d3aa621f4e
intotf <intotf@163.com>
2017-12-05 15:12:11 +0800

    增加web 和 tcp 通信

--- TcpSocket/Program.cs
+++ TcpSocket/Program.cs
@@ -1,19 +1,27 @@
-﻿using NetworkSocket.Fast;
+﻿using NetworkSocket.Core;
+using NetworkSocket.Fast;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace TcpSocket
 {
     class Program
     {
-        static Main(string[] args)
+        static void Main(string[] args)
         {
-            FastTcpClient client = new FastTcpClient();
-            client.Connect("10.0.3.88", 1850);
-
+            HostFactory.Run(c =>
+            {
+                c.Service<ListenerControl>();
+                c.RunAsLocalSystem();
+                c.SetServiceName("FastMessageService");
+                c.SetDisplayName("FastMessageService");
+                c.SetDescription("FastMessageService");
+            });
         }
     }
 }
